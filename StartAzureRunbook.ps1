6b52432bb928c2d69314042e94ae96fa117d5674
morneniemann <morneniemann@capitecbank.co.za>
2016-10-10 13:51:51 +0200

    Add files via upload

---
+++ StartAzureRunbook.ps1
@@ -0,0 +1,8 @@
+﻿
+
+Select-AzureSubscription -SubscriptionName Capitec-Sandbox-ITISAS
+$Params = @{"VM_Name" = $vm_name}
+Start-AzureAutomationRunbook -Name "CreateVM_Create_Linux_VM_New1" -AutomationAccountName "ITISAS" -RunOn "CapitecOnPremise" -Parameters $Params
+
+
+
