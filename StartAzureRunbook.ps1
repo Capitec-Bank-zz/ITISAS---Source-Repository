aad403cebf3a587704da1db1897e1f22a3772c38
morneniemann <morneniemann@capitecbank.co.za>
2016-10-10 15:21:58 +0200

    Update StartAzureRunbook.ps1

--- StartAzureRunbook.ps1
+++ StartAzureRunbook.ps1
@@ -5,4 +5,4 @@ $Params = @{"VM_Name" = $vm_name}
 Start-AzureAutomationRunbook -Name "CreateVM_Create_Linux_VM_New1" -AutomationAccountName "ITISAS" -RunOn "CapitecOnPremise" -Parameters $Params
 
 
-
+bla
